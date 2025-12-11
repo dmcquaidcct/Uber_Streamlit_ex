506a13205d4998437e4fb5dee2d5fd9de122d3fb
David McQuaid <dmcquaid@cct.ie>
2025-12-11 12:31:42 +0000

    Update uber.py

--- uber.py
+++ uber.py
@@ -23,3 +23,8 @@ data_load_state.text("Done!")
 if st.checkbox("Show Raw Data"):
     st.subheader("Raw Data")
     st.write(data)
+    
+st.subheader("Number of Pickups Per Hour")
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour,
+                bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
