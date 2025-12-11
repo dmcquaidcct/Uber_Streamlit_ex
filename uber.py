2ca8d69bee914e53a4c9e1bb9e0b29e7a1d2e3f4
David McQuaid <dmcquaid@cct.ie>
2025-12-11 12:26:05 +0000

    Update uber.py

--- uber.py
+++ uber.py
@@ -19,3 +19,7 @@ def load_data(nrows):
 data_load_state=st.text("Data Loading...")
 data=load_data(10000)
 data_load_state.text("Done!")
+
+if st.checkbox("Show Raw Data"):
+    st.subheader("Raw Data")
+    st.write(data)
